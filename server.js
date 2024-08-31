9eae0d89b58fc341cb17b2e6ce042a0e8b19cee8
Ítalo Falzoni <=>
2024-08-31 17:31:16 -0300

    build: adjust server.js to deploy

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ dotenv.config()
 
 import app from "./app.js"
 
-const port = process.env.PORT || 3000
-
 // Verificar se a string de conexão do MongoDB está definida
 if (!process.env.MONGODB_STRING_CONNECTION) {
   console.error("Erro: MONGODB_STRING_CONNECTION não está definida no arquivo .env")
@@ -17,8 +15,8 @@ mongoose.connect(process.env.MONGODB_STRING_CONNECTION)
   .then(() => {
     console.log("Conectado ao MongoDB")
     // Iniciar o servidor após a conexão com o banco de dados
-    app.listen(port, () => {
-      console.log(`Servidor rodando na porta ${port}`)
+    app.listen(process.env.PORT || 3000, function () {
+      console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     })
   })
   .catch((error) => {
